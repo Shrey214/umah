b9076f7f0a505627b170a0aae397e68145afe493
shreyas-2108 <roshanporwal63@gmail.com>
2024-04-16 19:14:33 +0530

    commit by shreyash

--- lib/screen/MainScreen/main_page.dart
+++ lib/screen/MainScreen/main_page.dart
@@ -13,46 +13,170 @@ class _MainPageState extends State<MainPage> {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false, // To remove the back arrow button
-        leading: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20), // Adjust the padding as needed
-          child: SizedBox(
-            height: 200,
-            width: 200,
+        title: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 1),
+          // Adjust the padding as needed
+          child: Align(
+            alignment: Alignment.centerLeft, // Align the title to the left
             child: Image.asset(
               'assets/image/mainPage_umah.png',
+              width: 100,
+              height: 22,
               fit: BoxFit.contain, // Adjust the fit as needed
             ),
           ),
         ),
         actions: [
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10), // Adjust the padding as needed
-            child: IconButton(
-              icon: Stack(
-                children: [
-                  const Icon(Icons.shopping_cart, size: 30), // Your desired icon size
-                  Positioned(
-                    top: 0,
-                    right: 0,
-                    child: Container(
-                      padding: const EdgeInsets.all(4),
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.red, // Change the color of the border as needed
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              // Adjust the padding as needed
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                      color: Colors.black.withOpacity(0.2), width: 1),
+                  // Light thin black border
+                  borderRadius: BorderRadius.circular(10), // Border radius
+                ),
+                child: IconButton(
+                  icon: Stack(
+                    children: [
+                      const Icon(Icons.shopping_cart, size: 30),
+                      // Your desired icon size
+                      Positioned(
+                        top: 0,
+                        right: 0,
+                        child: Container(
+                          padding: const EdgeInsets.all(4),
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors
+                                .red, // Change the color of the border as needed
+                          ),
+                          child: const Icon(Icons.notifications_none,
+                              size: 10,
+                              color: Colors.white), // Your notification icon
+                        ),
                       ),
-                      child: const Icon(Icons.notifications_none, size: 10, color: Colors.white), // Your notification icon
+                    ],
+                  ),
+                  onPressed: () {
+                    // Add onPressed callback functionality here
+                  },
+                ),
+              )),
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        // Align children to the start (left) of the column
+        children: [
+          // First row
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    color: const Color.fromRGBO(4, 19, 48, 1),
+                    height: 120,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset('assets/image/WalletIcon.png',color: Colors.deepOrangeAccent,),
+                        const SizedBox(width: 10,),
+                        const Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Wallet",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,),),
+                            Text("\$2,700", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),)
+
+                          ],
+                        ),
+                      ],
                     ),
                   ),
-                ],
-              ),
-              onPressed: () {
-                // Add onPressed callback functionality here
-              },
+                ),
+                Expanded(
+                  child: Container(
+                    color: const Color.fromRGBO(4, 19, 48, 1),
+                    height: 120,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset('assets/image/ahievement-icon.png',color: Colors.deepOrangeAccent,),
+                        const SizedBox(width: 10,),
+                        const Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Points",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,),),
+                            Text("4,500", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),)
+
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: const Color.fromRGBO(4, 19, 48, 1),
+                    height: 120,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset('assets/image/discount-icon.png',color: Colors.deepOrangeAccent,),
+                        const SizedBox(width: 10,),
+                        const Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Vouchers",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,),),
+                            Text("2", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),)
+
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
+          // Second row
+          Container(
+            color: Colors.red,
+            height: 100,
+            child: Center(
+              child: Text('Row 2'),
+            ),
+          ),
+          // Third row
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  color: Colors.yellow,
+                  height: 100,
+                  child: Center(
+                    child: Text('Row 3 - Item 1'),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.orange,
+                  height: 100,
+                  child: Center(
+                    child: Text('Row 3 - Item 2'),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ],
-      )
-
+      ),
     );
   }
 }
