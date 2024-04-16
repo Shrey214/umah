9b3d7bd34ac079689045266c577c71db71885af8
atul-patel-silver <atulpatel2001a@gmail.com>
2024-04-16 16:58:09 +0530

    Resolve merge conflict

--- lib/screen/splash/main_splash_screen.dart
+++ lib/screen/splash/main_splash_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:umah/screen/login_screen.dart';
 
 import '../home_screen.dart';
+
 class MainSplashScreen extends StatelessWidget {
   const MainSplashScreen({super.key});
 
@@ -8,12 +10,36 @@ class MainSplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     Future.delayed(const Duration(seconds: 3), () {
       Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (_) =>const HomeScreen()),
+        MaterialPageRoute(builder: (_) => const LoginScreen()),
       );
     });
     return Scaffold(
       body: Center(
-        child: Image.asset('assets/splash.png'), // Replace 'splash.png' with your actual image asset
+        child: Stack(
+          children: [
+            Positioned(
+              width: 84.37,
+              height: 70.15,
+              top: 246.5,
+              left: 152,
+              child: Image.asset('assets/image/umah-logo.png'),
+            ),
+            Positioned(
+              width: 425,
+              height: 530,
+              top: 265,
+              left: -25,
+              child: Image.asset('assets/image/chair-image.png'),
+            ),
+            Positioned(
+              width: 116,
+              height: 41.25,
+              top: 732,
+              left: 129,
+              child: Image.asset('assets/image/Copyright.png'),
+            ),
+          ],
+        ),
       ),
     );
   }
